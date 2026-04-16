1d06edf7f3e6b5a7c57c9bfb9cea5b7744d0ecc5
akrep17m-png <akr.ep17m@gmail.com>
2026-04-16 13:16:02 +0300

    feat: create js code for app

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,19 @@
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
-const gallery = document.querySelector('.gallery');
-const galleryMarkup = images
-  .map(
-    ({ preview, original, description }) => `<li class="gallery-item">
-  <a class="gallery-link" href="${original}">
-    <img
-      class="gallery-image"
-      src="${preview}"
-      alt="${description}"
-    />
-  </a>
-</li>
-`
-  )
-  .join('');
+const API_KEY = '55463829-87ababc408fcd8cb97ff0a765';
 
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+export async function getImagesByQuery(query, page = 1) {
+  const response = await axios.get('https://pixabay.com/api/', {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page,
+    },
+  });
 
-new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+  return response.data;
+}
